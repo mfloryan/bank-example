b2b88d17cb8a0447dbbd7650135085ba8d358c76
Tom Scott <tom.scott@bglgroup.co.uk>
2013-05-21 16:53:02 +0100

    Tom: AggregateRoots MK I - it's there & has a test! W00t!

--- src/CTM.Bank.Domain/Aggregates/Account.cs
+++ src/CTM.Bank.Domain/Aggregates/Account.cs
@@ -1,7 +1,15 @@
-﻿namespace CTM.Bank.Domain.Aggregates
+﻿using CTM.Bank.Domain.Events;
+using CTM.Domain.Core;
+
+namespace CTM.Bank.Domain.Aggregates
 {
-    public class Account
+    public class Account : AggregateRoot
     {
-         
+        public Account(AggregateDescriptor aggregateDescriptor) : base(aggregateDescriptor.Id)
+        {
+            RaiseEvent(new AccountCreated());
+        }
+
+        private void Apply(AccountCreated evt) {}
     }
 }
---
+++ src/CTM.Bank.Tests/Unit/Aggregates/AccountSpecification.cs
@@ -0,0 +1,17 @@
+﻿using CTM.Bank.Domain.Aggregates;
+using CTM.Domain.Core;
+using NUnit.Framework;
+
+namespace CTM.Bank.Tests.Unit.Aggregates
+{
+    [TestFixture]
+    public class AccountSpecification
+    {
+        [Test]
+        public void ShouldPublishAnAccountCreatedEvent()
+        {
+            var account = new Account(AggregateDescriptor.New());
+            Assert.That(account.UncommittedEvents.Count, Is.EqualTo(1));
+        }
+    }
+}
